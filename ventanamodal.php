5fa72bdc14cafe88ce366ea501cf22695c459ecd
Carlos cordova <45829368+CarlosCordova1@users.noreply.github.com>
2020-05-15 13:13:52 -0500

    php ventanamodal

---
+++ ventanamodal.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Plugin Name: Ventana Modal
+ * Plugin URI: www.carloscordova.com
+ * Description: un simple modal
+ * Version: 1.0
+ * Author: Carlos Cordova
+ * Author URI: https://github.com/CarlosCordova1
+ */
+add_action('wp_footer', 'modal');
+function modal()
+{
+  ?>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+ <script type="text/javascript">
+      jQuery(document).ready(function(e) {
+           $('#exampleModal').modal('show');
+         });
+ </script>
+
+  <?php
+}
